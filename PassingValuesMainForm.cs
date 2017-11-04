714d8dc9c9ffbc03ad6af74ce0da10b0be4c7bf5
Samarth Srivastava <samarth_srivastava@ymail.com>
2017-11-05 02:57:50 +0530

    First Commit

---
+++ PassingValuesMainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegatesEvents
+{
+    public partial class PassingValuesMainForm : Form
+    {
+        public PassingValuesMainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string sIdentifier = AssignValueToVariable();
+
+            CustomTypeEventArgs paramContainingUpdatedVariable = CustomTypeEventArgs.GetCustomTypeEventArgs();
+            paramContainingUpdatedVariable.stringProperty = sIdentifier;
+
+            PassingValuesSecondForm obj = new PassingValuesSecondForm();
+            obj.Show();
+        }
+
+        public string AssignValueToVariable()
+        {
+            string obj = "ABCDEFGHIKLMNOPQRSTUVWXYZ"; //use data source or any other way
+            return obj;
+        }
+    }
+
+    public class CustomTypeEventArgs : System.EventArgs
+    {
+        public static CustomTypeEventArgs instance = null;
+        private string str;
+
+        private CustomTypeEventArgs() { }
+
+        public static CustomTypeEventArgs GetCustomTypeEventArgs()
+        {
+            if (instance == null)
+                instance = new CustomTypeEventArgs();
+
+            return instance;
+        }
+
+        public string stringProperty
+        {
+            get { return str; }
+            set { str = value; }
+        }
+    }
+}
---
+++ PassingValuesSecondForm.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DelegatesEvents
+{
+    public partial class PassingValuesSecondForm : Form
+    {
+        string valueToBeAssigned = null;
+
+        public delegate void PassesValueHandler(object sender, CustomTypeEventArgs e);
+        public event PassesValueHandler passValueEvent;
+
+        public PassingValuesSecondForm()
+        {
+            InitializeComponent();
+
+            passValueEvent += PassingValuesSecondForm_passValueEvent;
+            passValueEvent(this, CustomTypeEventArgs.GetCustomTypeEventArgs());
+        }
+
+        public void PassingValuesSecondForm_passValueEvent(object sender, CustomTypeEventArgs e)
+        {
+            //this value which was loaded in the previous form is available to be used in this form from the start
+            //as the local variable was assigned the very same value as loaded in the previous form in the constructor of the form.
+            valueToBeAssigned = e.stringProperty;
+        }
+    }
+}
